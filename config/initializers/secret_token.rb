c09e16cfc4a59622a1a33be490ab687fa5219255
Kyrylo Silin <kyrylosilin@gmail.com>
2012-10-24 03:35:07 +0300

    Привет, мир!
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punchline::Application.config.secret_token = '7044a291cf951d20171ce7e710197d6cb26da6c2c78ebea08af85eb3570a5d39e592c57e7428441922bc0e09455260b5c68b7524d134eb010048cf77384b785a'
