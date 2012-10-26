a7032e10d49dfda1fc2149789bb3e714694b99cb
Kyrylo Silin <kyrylosilin@gmail.com>
2012-10-26 23:46:35 +0300

    Move error displaying code
    Move it from views to the new partial.
    
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # @param [ActiveModel::Errors] errors
+  # @return [Array<String>] the plain error messages
+  def error_messages_for(errors)
+    errors.values.flatten
+  end
+
 end
