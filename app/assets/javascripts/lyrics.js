a2716ba9af8050af4526d2e2087c9fcc61fc59d7
Kyrylo Silin <kyrylosilin@gmail.com>
2012-11-15 02:07:13 +0200

    Add rudimentary explain popup
    As a side effect, reorganise CSS.
    
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

--- app/assets/javascripts/lyrics.js
+++ app/assets/javascripts/lyrics.js
@@ -1,2 +1,58 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function toggleExplainPopup()
+{
+	var explainPopup = $('#explain_popup')
+
+	if (explainPopup.length == 0) {
+		showExplainPopup();
+	} else {
+		hideExplainPopup(explainPopup);
+	}
+}
+
+function showExplainPopup()
+{
+	var selection = rangy.getSelection(),
+	    selectionString = $.trim(selection.toString());
+
+	if (selectionString === '') {
+		return true;
+	}
+
+	if (selection.rangeCount > 0) {
+		var range = selection.getRangeAt(0).cloneRange(),
+		    explainPopup = $('<span>', { id: 'explain_popup', text: 'Объяснить'})[0];
+
+		range.collapse(false);
+		range.insertNode(document.createDocumentFragment().appendChild(explainPopup));
+	}
+}
+
+function hideExplainPopup(popup)
+{
+	popup.remove();
+}
+
+function hideExplainPopupOrDoNothing()
+{
+	var explainPopup = $('#explain_popup');
+
+	if (explainPopup) {
+		hideExplainPopup(explainPopup);
+	}
+}
+
+$(function() {
+	$('html').click(function (e) {
+		hideExplainPopupOrDoNothing();
+	});
+
+	$('#lyric').on({
+		click: function(e) {
+			e.stopPropagation();
+		},
+		mouseup: toggleExplainPopup
+	});
+});
